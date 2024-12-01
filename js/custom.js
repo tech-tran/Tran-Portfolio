be0f5795b0e621683ba5e92493d5647de4698d3e
tech-tran <kinhdoanhnhi654@gmail.com>
2024-12-01 19:00:36 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+new WOW().init(); 
+var typed = new Typed('.element', {
+ strings: ["Graphic Designer", "Web Designer","Tran Ngoc Phuong Tran"],
+ typeSpeed:50,
+ backSpeed:50,
+loop:true,
+});
+
+
